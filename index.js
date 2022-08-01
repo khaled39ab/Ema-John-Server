8864a4a088ba16fb82995dba20bdeae0519494db
khaled39ab <khaled39ab@gmail.com>
2022-08-01 22:53:43 +0600

    connecting with server

--- index.js
+++ index.js
@@ -6,3 +6,11 @@ const app = express();
 // middleware
 app.use(cors());
 app.use(express.json());
+
+app.get('/', (req, res) => {
+    res.send('Running ema-john on server');
+});
+
+app.listen(port, () =>{
+    console.log('ema-john is listening');
+});
